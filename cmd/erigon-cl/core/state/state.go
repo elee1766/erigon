2db0c18fb012639d903ba813ab14a37d1d77ebbc
a <a@tuxpa.in>
2023-05-03 12:40:57 -0500

    ok

--- cmd/erigon-cl/core/state/state.go
+++ cmd/erigon-cl/core/state/state.go
@@ -234,33 +234,50 @@ func (b *BeaconState) initBeaconState() error {
 	return nil
 }
 
+func (b *BeaconState) CopyInto(bs *BeaconState) (err error) {
+	if bs.BeaconState == nil {
+		bs.BeaconState = raw.New(b.BeaconConfig())
+	}
+	err = b.BeaconState.CopyInto(bs.BeaconState)
+	if err != nil {
+		return err
+	}
+	err = b.copyCachesInto(bs)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (b *BeaconState) copyCachesInto(bs *BeaconState) error {
+	if b.Version() == clparams.Phase0Version {
+		return bs.initBeaconState()
+	}
+	bs.publicKeyIndicies = make(map[[48]byte]uint64)
+	for pk, index := range b.publicKeyIndicies {
+		bs.publicKeyIndicies[pk] = index
+	}
+	// Sync caches
+	if err := bs.initCaches(); err != nil {
+		return err
+	}
+	copyLRU(bs.activeValidatorsCache, b.activeValidatorsCache)
+	copyLRU(bs.shuffledSetsCache, b.shuffledSetsCache)
+	copyLRU(bs.committeeCache, b.committeeCache)
+
+	if b.totalActiveBalanceCache != nil {
+		bs.totalActiveBalanceCache = new(uint64)
+		*bs.totalActiveBalanceCache = *b.totalActiveBalanceCache
+		bs.totalActiveBalanceRootCache = b.totalActiveBalanceRootCache
+	}
+	return nil
+}
+
 func (b *BeaconState) Copy() (bs *BeaconState, err error) {
-	newRaw, err := b.BeaconState.Copy()
+	copied := New(b.BeaconConfig())
+	err = b.CopyInto(copied)
 	if err != nil {
 		return nil, err
 	}
-	copied := NewFromRaw(newRaw)
-	if b.Version() == clparams.Phase0Version {
-		return copied, copied.initBeaconState()
-	}
-	copied.publicKeyIndicies = make(map[[48]byte]uint64)
-	for pk, index := range b.publicKeyIndicies {
-		copied.publicKeyIndicies[pk] = index
-	}
-	// Sync caches
-	if err := copied.initCaches(); err != nil {
-		return nil, err
-	}
-
-	copyLRU(copied.activeValidatorsCache, b.activeValidatorsCache)
-	copyLRU(copied.shuffledSetsCache, b.shuffledSetsCache)
-	copyLRU(copied.committeeCache, b.committeeCache)
-
-	if b.totalActiveBalanceCache != nil {
-		copied.totalActiveBalanceCache = new(uint64)
-		*copied.totalActiveBalanceCache = *b.totalActiveBalanceCache
-		copied.totalActiveBalanceRootCache = b.totalActiveBalanceRootCache
-	}
-
 	return copied, nil
 }
